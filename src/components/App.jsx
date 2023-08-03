2b5594246101902530e510708acc7e1fa438bbba
rene.ruano95@gmail.com <rene.ruano95@gmail.com>
2023-08-03 10:34:23 -0400

    third commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
-import { notes } from "../notes";
+import CreateArea from "./CreateArea";
+
 
 function App() {
     return(
         <div>
             <Header />
-            {notes.map((noteItem) => (
-                <Note 
-                    key={noteItem.key} 
-                    title={noteItem.title} 
-                    content={noteItem.content}/>
-                ))}
+            <CreateArea />
+            <Note />
             <Footer />
         </div>
     )
